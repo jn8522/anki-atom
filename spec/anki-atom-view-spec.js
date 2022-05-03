af09d9d6d8f6c8c0c56d6c3d500f08bfda811358
jn8522 <jnaylorpratt@gmail.com>
2022-05-03 20:53:00 +0800

    First commit

---
+++ spec/anki-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AnkiAtomView from '../lib/anki-atom-view';
+
+describe('AnkiAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
